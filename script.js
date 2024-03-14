0a87db5244d8c49925b767ed599bfff1b8db02ab
IsahMr <160750926+IsahMr@users.noreply.github.com>
2024-03-14 11:03:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function sendMessage() {
+    var userInput = document.getElementById("user-input").value;
+    if (userInput !== "") {
+        addUserMessage(userInput);
+        // Aqui você pode adicionar a lógica para processar a mensagem do usuário e obter a resposta do chatbot
+        // Por enquanto, vamos apenas simular uma resposta do chatbot
+        setTimeout(function() {
+            addBotMessage("Desculpe, ainda estou aprendendo. Não sei como responder a isso.");
+        }, 500);
+        document.getElementById("user-input").value = "";
+    }
+}
+
+function addUserMessage(message) {
+    var chatBox = document.getElementById("chat-box");
+    var userMessageElement = document.createElement("div");
+    userMessageElement.className = "user-message";
+    userMessageElement.textContent = message;
+    chatBox.appendChild(userMessageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function addBotMessage(message) {
+    var chatBox = document.getElementById("chat-box");
+    var botMessageElement = document.createElement("div");
+    botMessageElement.className = "bot-message";
+    botMessageElement.textContent = message;
+    chatBox.appendChild(botMessageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
